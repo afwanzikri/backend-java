3d3575c5a6df532ed003d0880b2eb22b96ab551e
afwanz <afwanzikri@gmail.com>
2024-03-18 16:03:44 +0700

    add binary search tree

---
+++ src/com/backendjava/datastructure/BinarySearchTree.java
@@ -0,0 +1,71 @@
+package com.backendjava.datastructure;
+
+class Node {
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+public class BinarySearchTree {
+    Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    // Method to insert a new key in the BST
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    // A recursive function to insert a new key in BST
+    Node insertRec(Node root, int key) {
+        // If the tree is empty, return a new node
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        // Otherwise, recur down the tree
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        // return the (unchanged) node pointer
+        return root;
+    }
+
+    // Method to perform inorder traversal of BST
+    void inorder() {
+        inorderRec(root);
+    }
+
+    // A utility function to do inorder traversal of BST
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.key + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        // Inserting elements
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // Printing inorder traversal of the BST
+        tree.inorder();
+    }
+}
